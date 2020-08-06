ddf67d27f4ec9bcda74ae735b51ff378664a5302
Kousuke_Minomo <kousuke_minomo@sobal.co.jp>
2020-08-06 11:18:56 +0900

    addFile

--- Phase2/introduction .cs
+++ Phase2/introduction .cs
@@ -17,7 +17,6 @@ namespace WindowsFormsApp1
         private int displayHeight = Screen.PrimaryScreen.Bounds.Height;
         //ディスプレイの幅
         private int displayWidh = Screen.PrimaryScreen.Bounds.Width;
-
         public introduction()
         {
             InitializeComponent();
