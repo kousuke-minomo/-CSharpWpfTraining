772e9585b547e56ece95a02da3fff2885079a33c
Kousuke_Minomo <kousuke_minomo@sobal.co.jp>
2020-09-02 09:46:11 +0900

    chngeFanction

---
+++ Phase3/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media;
+using System.Globalization;
+
+namespace Phase3
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+
+    public class ColorConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            byte R = (byte)(double)(values[0]);
+            byte G = (byte)(double)(values[1]);
+            byte B = (byte)(double)(values[2]);
+
+            return Color.FromRgb(R, G, B);
+        }
+
+        //-----------------------------------------------------------------------------------------------
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
---
+++ Phase4App/SizeConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Pase4
+{
+    class SizeConverter : IValueConverter
+    {
+        private static readonly string[] sizeStr = {"","Small", "Middle", "Large" };
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return sizeStr[(byte)(double)value];
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
